378b4ea8a7e617874d915794a9b13922fd00f09c
finaMarin <adam25fmarin@inslaferreria.cat>
2026-09-18 17:52:45 +0200

    Add overloaded suma methods for int, float, and String

--- src/Suma.java
+++ src/Suma.java
@@ -1,1 +1,13 @@
+class Suma {
+public static int suma(int a, int b) {
+return a+b;
+  }
 
+public static float suma(float a, float b){
+return a+b;
+ }
+
+public static suma (String a, String b){
+return a+b;
+  }
+}
